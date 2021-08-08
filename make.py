9e6f20961de2077790356b8fea9be3acba896fb5
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2021-08-08 20:32:56 +0300

    assets/alt_region_names.json is hardcoded

--- make.py
+++ make.py
@@ -1,7 +1,6 @@
 """Cоздать файлы:
    _towns.csv
    assets/regions.csv
-   assets/alt_region_names.json
 """
 
 #%%
@@ -25,23 +24,3 @@ filename = local_path("regions.csv")
 rf = df[["region_name"]].drop_duplicates().sort_values("region_name")
 rf.to_csv(filename, header=False, index=False, quoting=csv.QUOTE_ALL)
 print(f"Created {filename}")
-
-# %%
-from helper import local_path, to_json
-
-# В основе - "coord_dadata.csv"
-# cf.city != cf.query_city
-alt_city_names = {
-    "Дмитриев-Льговский": "Дмитриев",
-    "Белоозерский": "Белоозёрский",
-    "Королев": "Королёв",
-    "Ликино-Дулево": "Ликино-Дулёво",
-    "Озеры": "Озёры",
-    "Щелково": "Щёлково",
-    "Орел": "Орёл",
-}
-path = local_path("alt_city_names.json")
-to_json(alt_city_names, path)
-print(f"Created {path}")
-
-# %%
